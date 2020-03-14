b51705131d9dd2f4c650bf5536a369d90043eee7
Michel Deziderio <michel.silva02@gmail.com>
2020-03-14 04:06:25 -0300

    Iniciando a função de formatar nome dos authores

--- src/app/author/author.component.ts
+++ src/app/author/author.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 
 @Component({
   selector: 'app-author',
@@ -8,11 +9,14 @@ import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 })
 export class AuthorComponent implements OnInit {
 
-  visible: boolean = true;
+  visibleForm1: boolean = true;
+  visibleForm2: boolean = false;
   getNumbers: FormGroup;
   numberFils: number = 0;
   filds = [];
   close: boolean = false;
+  authors: Array<any>;
+  showAuthors: boolean = false;
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -21,15 +25,17 @@ export class AuthorComponent implements OnInit {
     this.getNumbers = this.formBuilder.group({
       numbers: [null]
     })
+
   }
 
   getNumberFilds(val) {
 
     this.numberFils = val.value.numbers;
 
-    if (this.numberFils < 10) {
+    if (this.numberFils <= 10) {
       this.generateFilds();
-      this.visible = false;
+      this.visibleForm1 = false;
+      this.visibleForm2 = true;
     }
     else {
       this.close = true;
@@ -38,18 +44,29 @@ export class AuthorComponent implements OnInit {
   }
 
   generateFilds() {
-    
     for (let i = 0; i < this.numberFils; i++) {
       this.filds.push({
         id: i,
         author: ''
       })
     }
-
   }
 
-  closeAlert(){
+  getNameAuthors() {
+
+    this.showAuthors = true;
+    this.visibleForm1 = false;
+    this.visibleForm2 = false;
+
+      return this.filds
+  }
+  
+  closeAlert() {
     this.close = false;
   }
 
+  removeList(){
+    this.showAuthors = false;
+    this.visibleForm1 = true;
+  }
 }
--- src/app/author/list-authors/list-authors.component.ts
+++ src/app/author/list-authors/list-authors.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-list-authors',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListAuthorsComponent implements OnInit {
 
+  @Input() filds: any;
+
+  yesNames = ['Neto', 'Neta', 'Filho', 'Filha', 'Sobrinho', 'Sobrinha', 'Junior'];
+  noNames = ['da', 'de', 'do', 'das', 'dos'];
+  authorNameFormater = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.getNames()
+  }
+  
+  getNames() {
+
+    this.filds.forEach(element => {
+      this.filds = [];
+      
+      this.filds.push({
+        author:element.author
+      })
+      
+    });
   }
 }
