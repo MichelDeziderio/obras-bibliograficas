a15d521d4e16479c40d8b3c19fe6079b008e8979
Michel Deziderio <michel.silva02@gmail.com>
2020-03-13 23:13:27 -0300

    Criando component e rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthorComponent } from './author/author.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component : AuthorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthorComponent } from './author/author.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorComponent
   ],
   imports: [
     BrowserModule,
